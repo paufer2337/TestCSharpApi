949152638602ea734f496463bd5b3468aa9bf462
Ironboy <ironboy@users.noreply.github.com>
2024-04-08 05:20:57 +0200

    initial playaround dynamic ii

--- Backend/src/App.cs
+++ Backend/src/App.cs
@@ -1,4 +1,4 @@
-var serverName = "ironboy's minimal API server";
+/*var serverName = "ironboy's minimal API server";
 var isSpa = true;
 Debug.on = true;
 CheckAcl.on = true;
@@ -35,4 +35,108 @@ CheckAcl.Start();
 Session.DeleteOldSessions(2);
 
 // Start the server on port 3001
-app.Run("http://localhost:3001");
+app.Run("http://localhost:3001");*/
+
+/*using ExtensionMethods;
+
+var a = new
+{
+    firstName = "Olle",
+    lastName = "Svensson",
+    cool = true,
+    age = 42,
+    weigth = 80.5
+}.Dynamic();
+
+Console.WriteLine(a.GetStr("firstName"));
+
+a.Set("middleName", "Afzelius");
+a.Set("middleName", "Dum");
+//a.Delete("lastName");
+
+Console.WriteLine(a.GetStr("lastName"));
+
+
+Console.WriteLine(a.GetInt("age"));
+
+Console.WriteLine(a.GetDbl("weigth"));
+
+var arr = new[] {
+    new { firstName = "Olle" },
+    new { firstName = "Anna" }
+}.Dynamic();
+
+arr.Push(
+    new { firstName = "Olga" },
+    new { firstName = "Agneta" }
+);
+
+Console.WriteLine(arr);*/
+
+using System.Linq.Expressions;
+using AgileObjects.ReadableExpressions;
+using ExtensionMethods;
+
+dynamic a = new Dynamic(new { job = "CEO", salary = 50000 });
+
+a.Merge(new { footPrint = "large", height = 1.84 });
+
+a.firstName = "Thomas";
+a.lastName = "Frank";
+a["middleName"] = "Irons";
+a["middleName"] = "ironboy";
+
+a.Delete("footPrint");
+
+Log.Out(a.middleName);
+Log.Out(a["lastName"]);
+
+Log.Out(a);
+
+dynamic b = new Dynamic(new[] { "Hej", "Hejdå" });
+
+b.Push("Really hejdå", "For the last time");
+
+b.Unshift("Prehej", "Almost ready for hej");
+
+Log.Out("POP", b.Pop());
+Log.Out("SHIFT", b.Shift());
+
+Log.Out("WHOLE", b);
+Log.Out("SLICE", b.Slice());
+Log.Out("SLICE1", b.Slice(1));
+Log.Out("SLICE1,3", b.Slice(1, 3));
+b.Reverse();
+Log.Out("REVERSED", b);
+Log.Out("SLICE(1,3)", b.Slice(1, 3));
+
+
+
+//Console.WriteLine(b.Map(x => x));
+
+//Log.Out("Map", Dynamic.Map((object)b, x => x + "hej"));
+
+/*Log.Out((b as object).Map((x, i) => x + "hej" + i));
+Log.Out((b as object).Filter(x => ((string)x).Contains("Almost")));*/
+
+
+// var eh = Dynamic.Mappy(x => x + "hohoho")
+/*Log.Out("MAP", b.Do(Dynamic.Map((x, i) => (x + "") + i + " IS DA BEST")));
+Log.Out("FILTER", b.Do(Dynamic.Filter(x => (x + "").Contains("hejdå"))));
+
+
+Log.Out("CHAIN",
+    b
+        .Do(Dynamic.Map((x, i) => (x + "") + i + " IS DA BEST"))
+        .Do(Dynamic.Filter(x => (x + "").Contains("hejdå")))
+);*/
+
+//Log.Out(b.Map("(x,i)=>x+i+'hejsan'").Filter("x=>x.Contains('3')"));
+
+var p = Dynamic.Prepare;
+var searchFor = "då";
+
+Log.Out("MAP+FILTER", b
+    .Map(p((string x, int i) => x + i + "hej"))
+    .Filter(p((string x) => x.Contains(searchFor), new { searchFor }))
+);
--- Backend/src/StringUtils.cs
+++ Backend/src/StringUtils.cs
@@ -1,5 +1,11 @@
+using System.Linq.Expressions;
 using System.Text.RegularExpressions;
-using System.ComponentModel;
+using AgileObjects.ReadableExpressions;
+
+//using System.ComponentModel;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+//using System.Data;
 
 namespace ExtensionMethods
 {
@@ -32,11 +38,190 @@ namespace ExtensionMethods
     public static class ObjectUtils
     {
 
+        /*public static object GetKeys(this object obj)
+        {
+            var descs = new List<String>();
+            foreach (var desc in TypeDescriptor.GetProperties(obj))
+            {
+                descs.Add(((PropertyDescriptor)desc).DisplayName);
+            }
+            Console.WriteLine(JsonConvert.DeserializeObject(JSON.Stringify(descs)).GetType());
+            return JsonConvert.DeserializeObject(JSON.Stringify(descs))!;
+        }
+
         public static object? GetObj(this object obj, string prop)
         {
-            PropertyDescriptorCollection properties = TypeDescriptor.GetProperties(obj);
-            PropertyDescriptor desc = properties.Find(prop, false)!;
+            var properties = TypeDescriptor.GetProperties(obj);
+            var desc = properties.Find(prop, false)!;
             return desc != null ? desc.GetValue(obj)! : null;
+        }*/
+
+        public static JObject Dynamic(this object obj)
+        {
+            return (JObject)JsonConvert.DeserializeObject(JSON.Stringify(obj))!;
+        }
+
+        public static void Delete(this JObject obj, string prop)
+        {
+            obj.Remove(prop);
+        }
+
+        public static void Set(this JObject obj, string prop, object value)
+        {
+            obj.Delete(prop);
+            obj.Add(prop, new JValue(value));
+        }
+
+        public static JObject GetObj(this JObject obj, string prop)
+        {
+            return obj.GetValue(prop)!.Dynamic();
+        }
+
+        public static string GetStr(this JObject obj, string prop)
+        {
+            return (string)obj.GetValue(prop)!;
+        }
+
+        public static int GetInt(this JObject obj, string prop)
+        {
+            return (int)obj.GetValue(prop)!;
+        }
+
+        public static decimal GetDec(this JObject obj, string prop)
+        {
+            return (decimal)obj.GetValue(prop)!;
+        }
+
+        public static double GetDbl(this JObject obj, string prop)
+        {
+            return (double)obj.GetValue(prop)!;
+        }
+
+        public static bool GetBool(this JObject obj, string prop)
+        {
+            return (bool)obj.GetValue(prop)!;
+        }
+
+    }
+
+    public static class ArrayUtils
+    {
+
+        public static JArray Dynamic(this Array arr)
+        {
+            return (JArray)JsonConvert.DeserializeObject(JSON.Stringify(arr))!;
+        }
+
+        public static int Length(this JArray arr)
+        {
+            return arr.Count();
+        }
+
+        public static JArray Splice(
+            this JArray arr, int start, int deleteCount, params object[] values
+        )
+        {
+            start = start < 0 ? arr.Count() - start : start;
+            start = start < 0 ? 0 : start;
+            var removed = new JArray();
+            for (var i = 0; i < deleteCount && arr.Length() - 1 >= start; i++)
+            {
+                removed.Add(arr[start]);
+                arr.RemoveAt(start);
+            }
+            for (var i = values.Length - 1; i >= 0; i--)
+            {
+                arr.Insert(start, values[i].Dynamic());
+            }
+            return removed;
+        }
+
+        public static int Push(this JArray arr, params object[] values)
+        {
+            arr.Splice(arr.Length(), 0, values);
+            return arr.Length();
+        }
+
+        public static int Unshift(this JArray arr, params object[] values)
+        {
+            arr.Splice(0, 0, values);
+            return arr.Length();
+        }
+
+        public static JObject Pop(this JArray arr)
+        {
+            return arr.Splice(arr.Length() - 1, 1)[0].Dynamic();
+        }
+
+        public static JObject Shift(this JArray arr)
+        {
+            return arr.Splice(0, 1)[0].Dynamic();
+        }
+
+        public static JObject GetObj(this JArray arr, int index, string prop)
+        {
+            return arr[index].Dynamic().GetObj(prop);
+        }
+
+        public static string GetStr(this JArray arr, int index, string prop)
+        {
+            return arr[index].Dynamic().GetStr(prop);
+        }
+
+        public static int GetInt(this JArray arr, int index, string prop)
+        {
+            return arr[index].Dynamic().GetInt(prop);
+        }
+
+        public static decimal GetDec(this JArray arr, int index, string prop)
+        {
+            return arr[index].Dynamic().GetDec(prop);
+        }
+
+        public static double GetDbl(this JArray arr, int index, string prop)
+        {
+            return arr[index].Dynamic().GetDbl(prop);
+        }
+
+        public static bool GetBool(this JArray arr, int index, string prop)
+        {
+            return arr[index].Dynamic().GetBool(prop);
+        }
+
+        private static void DynThrower(object obj, string methodName)
+        {
+            if (obj as Dynamic != null) { return; }
+            throw new Exception($"The {methodName} method is meant " +
+                "for use with Dynamic instances only!");
+        }
+
+        public static Dynamic Map(this object obj, Func<object, object> lambda)
+        {
+            DynThrower(obj, "Map");
+            return new Dynamic(((Dynamic)obj).ToArray().Select(lambda));
+        }
+
+        public static Dynamic Map(this object obj, Func<object, int, object> lambda)
+        {
+            DynThrower(obj, "Map");
+            return new Dynamic(((Dynamic)obj).ToArray().Select(lambda));
+        }
+
+        public static Dynamic Filter(this object obj, Func<object, bool> lambda)
+        {
+            DynThrower(obj, "Filter");
+            return new Dynamic(((Dynamic)obj).ToArray().Where(lambda));
+        }
+
+        public static Dynamic Filter(this object obj, Func<object, int, bool> lambda)
+        {
+            DynThrower(obj, "Filter");
+            return new Dynamic(((Dynamic)obj).ToArray().Where(lambda));
+        }
+
+        public static String _(this Expression expression)
+        {
+            return expression.ToReadableString();
         }
 
     }
