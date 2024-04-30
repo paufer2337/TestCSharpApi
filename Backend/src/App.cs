64b37b263db4f21b71721b95e50d82dc5fdaad6f
Eric Browne <ericbrowne@hotmail.com>
2024-04-30 22:00:18 +0200

    Added "EricBee Minimal Techno API Server" name

--- Backend/src/App.cs
+++ Backend/src/App.cs
@@ -6,7 +6,7 @@ Globals = Obj(new
     aclOn = true,
     isSpa = true,
     port = 3001,
-    serverName = "Ironboy's Minimal API Server",
+    serverName = "EricBee Minimal Techno API Server",
     frontendPath = Path.Combine("..", "Frontend"),
     sessionLifeTimeHours = 2
 });
