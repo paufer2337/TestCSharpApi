7efa3082b1e5d37850ef4bf1e40bcfebc236ca17
paufer2337 <paufer2337@gmail.com>
2024-07-19 16:38:29 +0200

    corrected errors with step definitions

--- ui-tests/specs/step_definitions/correct-products.js
+++ ui-tests/specs/step_definitions/correct-products.js
@@ -1,6 +1,6 @@
 import { Given, When, Then } from "@badeball/cypress-cucumber-preprocessor";
 
-Given('that I am on the product page', () => {
+Given('I am on the product page', () => {
   cy.visit('/products');
 });
 
